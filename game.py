aafbc8e5f8e177bcaa6ae06399d3107e01786c08
Moritz <schmoetzer.moritz@gmail.com>
2020-08-11 20:11:21 +0200

    Added game.py
    Thats's the code of the "Rock-Paper-Scissors" project.

---
+++ game.py
@@ -0,0 +1,8 @@
+user_input = input()
+
+if user_input == "rock":
+    print("Sorry, but computer chose paper")
+elif user_input == "paper":
+    print("Sorry, but computer chose scissors")
+elif user_input == "scissors":
+    print("Sorry, but computer chose rock")
