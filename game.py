0dd2b37b9c7613d890cc7c9292a1f6c9cc208a98
Moritz <schmoetzer.moritz@gmail.com>
2020-08-12 18:12:02 +0200

    Code of Stage 2/5
    That's my code of stage 2/5 of the "Rock-Paper-Scissor" project.

--- game.py
+++ game.py
@@ -1,8 +1,18 @@
-user_input = input()
+from random import choice
 
-if user_input == "rock":
-    print("Sorry, but computer chose paper")
-elif user_input == "paper":
-    print("Sorry, but computer chose scissors")
-elif user_input == "scissors":
-    print("Sorry, but computer chose rock")
+
+class RPS:
+    cpu = choice(["rock", "paper", "scissors"])
+    beats = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
+
+    def game(self, user_choice):
+        if user_choice == self.cpu:
+            print(f"There is a draw {self.cpu}")
+        elif self.beats[user_choice] == self.cpu:
+            print(f"Well done. Computer chose {self.cpu} and failed")
+        else:
+            print(f"Sorry, but computer chose {self.cpu}")
+
+
+game = RPS()
+game.game(input())
