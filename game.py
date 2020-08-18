69fa6c312307fde55de7d4a94e083c883b5d5e69
Moritz <schmoetzer.moritz@gmail.com>
2020-08-18 18:06:35 +0200

    Code of Stage 4/5
    That's my code of stage 4/5 of the "Rock-Paper-Scissor" project.

--- game.py
+++ game.py
@@ -5,24 +5,42 @@ class RPS:
     beats = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
     choices = ["rock", "paper", "scissors"]
 
-    def game(self, user_choice):
-        cpu = choice(self.choices)
-        if user_choice not in self.choices:
-            print("Invalid input!")
-            pass
-        else:
-            if user_choice == cpu:
-                print(f"There is a draw {cpu}")
-            elif self.beats[user_choice] == cpu:
-                print(f"Well done. Computer chose {cpu} and failed")
+    def __init__(self):
+        self.points = 0
+
+    def welcome(self):
+        user_name = input("Enter your name: ")
+        print(f"Hello, {user_name}")
+        file = open("rating.txt", "r+")
+        for line in file:
+            if user_name in line:
+                self.points = int(line.split()[1])
+        file.close()
+
+    def game(self):
+        while True:
+            user_input = input()
+            if user_input == "!exit":
+                print("Bye!")
+                break
+            elif user_input == "!rating":
+                print(self.points)
+                continue
             else:
-                print(f"Sorry, but computer chose {cpu}")
+                cpu = choice(self.choices)
+                if user_input not in self.choices:
+                    print("Invalid input!")
+                else:
+                    if user_input == cpu:
+                        print(f"There is a draw {cpu}")
+                        self.points += 50
+                    elif self.beats[user_input] == cpu:
+                        print(f"Well done. Computer chose {cpu} and failed")
+                        self.points += 100
+                    else:
+                        print(f"Sorry, but computer chose {cpu}")
 
 
 game = RPS()
-while True:
-    user_input = input()
-    if user_input == "!exit":
-        print("Bye!")
-        break
-    game.game(user_input)
+game.welcome()
+game.game()
